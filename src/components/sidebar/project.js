5a2822e0caa9932f85b778ece1c6ab57f8344cff
Shubham Raj <shubham14p3@gmail.com>
2020-07-16 01:14:47 +0530

    add src/component

--- src/components/sidebar/project.js
+++
@@ -1,8 +0,0 @@
-import utils from '../utils';
-import pasteProjects from './sidebarUtils';
-
-const projects = utils.make('ul', 'project-container');
-
-pasteProjects(projects);
-
-export default projects;
